19161c6de570275998ad80c878e82f8fc2d06317
rankell <149018923+rankell@users.noreply.github.com>
2025-04-23 10:49:07 -0300

    Create banco-pessoal.sql

---
+++ banco-pessoal.sql
@@ -0,0 +1,7 @@
+CREATE TABLE robloz (
+    Codigo_robloz INT PRIMARY KEY,
+    Nome_robloz VARCHAR(100) NOT NULL,
+    Preco_robux_robloz DECIMAL(10, 2) NOT NULL,
+    Noticias TEXT
+);
+SELECT * FROM robloz;
